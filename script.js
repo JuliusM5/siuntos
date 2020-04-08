925eb35ff0861d5413695fd75a2bbff0f304d410
JuliusM5 <54817220+JuliusM5@users.noreply.github.com>
2020-04-08 18:27:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-window.onscroll = function () {
-  myFunction();
-};
-
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
 $(document).ready(function () {
   // Add smooth scrolling to all links
   $("a").on("click", function (event) {
