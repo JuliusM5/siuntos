13f2db28816b2728aa7ebbed401ba74eb1f1003c
JuliusM5 <54817220+JuliusM5@users.noreply.github.com>
2020-04-15 15:05:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
+// following header contacts text
+$(window).scroll(function () {
+  var sticky = $(".first-header"),
+    scroll = $(window).scrollTop();
+
+  if (scroll >= 100) sticky.addClass("fixed");
+  else sticky.removeClass("fixed");
+});
+// following header contacts text end
+
 // function for scrolling
 
-$(document).ready(function () {
-  // Add smooth scrolling to all links
-  $("a").on("click", function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+// $(document).ready(function () {
+// Add smooth scrolling to all links
+// $("a").on("click", function (event) {
+// Make sure this.hash has a value before overriding default behavior
+// if (this.hash !== "") {
+// Prevent default anchor click behavior
+// event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+// Store hash
+// var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        800,
-        function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    } // End if
-  });
-});
+// Using jQuery's animate() method to add smooth page scroll
+// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+// $("html, body").animate(
+// {
+// scrollTop: $(hash).offset().top,
+// },
+// 800,
+// function () {
+// Add hash (#) to URL when done scrolling (default click behavior)
+// window.location.hash = hash;
+// }
+// );
+// } // End if
+// });
+// });
 // function for scrolling end
 
 // button for moving to top
@@ -53,3 +63,151 @@ function topFunction() {
 $(function () {
   $("#header").load("head.html");
 });
+$(document).ready(function () {
+  var itemsMainDiv = ".MultiCarousel";
+  var itemsDiv = ".MultiCarousel-inner";
+  var itemWidth = "";
+
+  $(".leftLst, .rightLst").click(function () {
+    var condition = $(this).hasClass("leftLst");
+    if (condition) click(0, this);
+    else click(1, this);
+  });
+
+  ResCarouselSize();
+
+  $(window).resize(function () {
+    ResCarouselSize();
+  });
+
+  //this function define the size of the items
+  function ResCarouselSize() {
+    var incno = 0;
+    var dataItems = "data-items";
+    var itemClass = ".item";
+    var id = 0;
+    var btnParentSb = "";
+    var itemsSplit = "";
+    var sampwidth = $(itemsMainDiv).width();
+    var bodyWidth = $("body").width();
+    $(itemsDiv).each(function () {
+      id = id + 1;
+      var itemNumbers = $(this).find(itemClass).length;
+      btnParentSb = $(this).parent().attr(dataItems);
+      itemsSplit = btnParentSb.split(",");
+      $(this)
+        .parent()
+        .attr("id", "MultiCarousel" + id);
+
+      if (bodyWidth >= 1200) {
+        incno = itemsSplit[5];
+        itemWidth = sampwidth / incno;
+      } else if (bodyWidth >= 992) {
+        incno = itemsSplit[2];
+        itemWidth = sampwidth / incno;
+      } else if (bodyWidth >= 768) {
+        incno = itemsSplit[1];
+        itemWidth = sampwidth / incno;
+      } else {
+        incno = itemsSplit[0];
+        itemWidth = sampwidth / incno;
+      }
+      $(this).css({
+        transform: "translateX(0px)",
+        width: itemWidth * itemNumbers,
+      });
+      $(this)
+        .find(itemClass)
+        .each(function () {
+          $(this).outerWidth(itemWidth);
+        });
+
+      $(".leftLst").addClass("over");
+      $(".rightLst").removeClass("over");
+    });
+  }
+
+  //this function used to move the items
+  function ResCarousel(e, el, s) {
+    var leftBtn = ".leftLst";
+    var rightBtn = ".rightLst";
+    var translateXval = "";
+    var divStyle = $(el + " " + itemsDiv).css("transform");
+    var values = divStyle.match(/-?[\d\.]+/g);
+    var xds = Math.abs(values[4]);
+    if (e == 0) {
+      translateXval = parseInt(xds) - parseInt(itemWidth * s);
+      $(el + " " + rightBtn).removeClass("over");
+
+      if (translateXval <= itemWidth / 2) {
+        translateXval = 0;
+        $(el + " " + leftBtn).addClass("over");
+      }
+    } else if (e == 1) {
+      var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+      translateXval = parseInt(xds) + parseInt(itemWidth * s);
+      $(el + " " + leftBtn).removeClass("over");
+
+      if (translateXval >= itemsCondition - itemWidth / 2) {
+        translateXval = itemsCondition;
+        $(el + " " + rightBtn).addClass("over");
+      }
+    }
+    $(el + " " + itemsDiv).css(
+      "transform",
+      "translateX(" + -translateXval + "px)"
+    );
+  }
+
+  //It is used to get some elements from btn
+  function click(ell, ee) {
+    var Parent = "#" + $(ee).parent().attr("id");
+    var slide = $(Parent).attr("data-slide");
+    ResCarousel(ell, Parent, slide);
+  }
+});
+var theToggle = document.getElementById("toggle");
+
+// based on Todd Motto functions
+// https://toddmotto.com/labs/reusable-js/
+
+// hasClass
+function hasClass(elem, className) {
+  return new RegExp(" " + className + " ").test(" " + elem.className + " ");
+}
+// addClass
+function addClass(elem, className) {
+  if (!hasClass(elem, className)) {
+    elem.className += " " + className;
+  }
+}
+// removeClass
+function removeClass(elem, className) {
+  var newClass = " " + elem.className.replace(/[\t\r\n]/g, " ") + " ";
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(" " + className + " ") >= 0) {
+      newClass = newClass.replace(" " + className + " ", " ");
+    }
+    elem.className = newClass.replace(/^\s+|\s+$/g, "");
+  }
+}
+// toggleClass
+function toggleClass(elem, className) {
+  var newClass = " " + elem.className.replace(/[\t\r\n]/g, " ") + " ";
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(" " + className + " ") >= 0) {
+      newClass = newClass.replace(" " + className + " ", " ");
+    }
+    elem.className = newClass.replace(/^\s+|\s+$/g, "");
+  } else {
+    elem.className += " " + className;
+  }
+}
+
+theToggle.onclick = function () {
+  toggleClass(this, "on");
+  return false;
+};
+// document.querySelector(".love").scrollIntoView({
+//   behavior: "smooth",
+// });
---
+++ slider.js
@@ -0,0 +1,8 @@
+$(document).on("ready", function () {
+  $(".regular").slick({
+    dots: false,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });
+});
